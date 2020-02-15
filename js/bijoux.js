06c428d80c5e03b47ef96081e6c6e00ddbcdc088
Kindeme <fkounoudji@gmail.com>
2020-02-15 09:23:27 -0600

    index style

---
+++ js/bijoux.js
@@ -0,0 +1,12 @@
+// hide preloader
+// all the images scripts links have finished loading
+
+// window event list
+window.addEventListener('load',function(){
+  document.querySelector('.preloader').style.display = 'none';
+});
+//nav btn
+document.querySelector('.header__navBtn').addEventListener('click',function(){
+  document.querySelector('.header__nav')
+
+})
