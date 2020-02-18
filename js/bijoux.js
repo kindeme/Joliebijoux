7d9b7a29415b1bd2ba7f01609421388d580548b2
Kindeme <fkounoudji@gmail.com>
2020-02-17 21:40:17 -0600

    index style

--- js/bijoux.js
+++ js/bijoux.js
@@ -2,11 +2,27 @@
 // all the images scripts links have finished loading
 
 // window event list
+
+eventListener();
+function eventListener(){
+  const ui = new UI();
 window.addEventListener('load',function(){
-  document.querySelector('.preloader').style.display = 'none';
+ ui.hidePreloader();
 });
 //nav btn
 document.querySelector('.header__navBtn').addEventListener('click',function(){
-  document.querySelector('.header__nav').classList.toggle('nav__show');
-
+ ui.showNav();
 })
+}
+//  UI constructor
+function UI(){}
+//  hide function
+UI.prototype.hidePreloader = function(){
+  document.querySelector('.preloader').style.display = 'none';
+}
+//  show function 
+UI.prototype.showNav = function(){
+  document.querySelector('.header__nav').classList.toggle('nav__show');
+}
+// control the video
+// document.querySelector('.header__switch-container__video__switch').addEventListener('click',function({})
